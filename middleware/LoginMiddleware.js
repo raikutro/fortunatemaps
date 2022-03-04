835cd4f8b15badde4cd37a971b0b75e8c2effcc4
Electro <hydrosaur45@gmail.com>
2022-03-04 13:08:49 -0800

    pass-over

--- middleware/LoginMiddleware.js
+++ middleware/LoginMiddleware.js
@@ -3,26 +3,29 @@ const SETTINGS = require('../Settings');
 
 module.exports = (loginTokens) => {
 	return (req, res, next) => {
-		req.profileID = false;
-		req.getProfile = () => null;
+		console.log(process.env.PROFILE_ID_OVERRIDE, loginTokens);
 
 		if(process.env.PROFILE_ID_OVERRIDE) {
 			req.profileID = process.env.PROFILE_ID_OVERRIDE;
 		} else if(loginTokens[req.cookies[SETTINGS.SITE.COOKIE_TOKEN_NAME]]){
 			req.loginToken = loginTokens[req.cookies[SETTINGS.SITE.COOKIE_TOKEN_NAME]];
 			req.profileID = loginTokens[req.cookies[SETTINGS.SITE.COOKIE_TOKEN_NAME]].profileID;
+		} else {
+			req.profileID = false;
 		}
 
-		if(!req.profileID) return next();
-
 		req.getProfile = async (useDocument) => {
-			if(!req.profileData) req.profileData = await User.findById(req.profileID)
-			.then(doc => useDocument ? doc : doc.toObject())
-			.catch(err => {
-				console.error(err);
+			if(!req.profileID) {
+				req.profileData = null;
+			} else if(!req.profileData) {
+				req.profileData = await User.findById(req.profileID)
+				.then(doc => useDocument ? doc : doc.toObject())
+				.catch(err => {
+					console.error(err);
 
-				return null;
-			});
+					return null;
+				});
+			}
 
 			return req.profileData;
 		};
